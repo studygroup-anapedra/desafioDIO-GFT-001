5cb5fa52b30deb13f4f0666feb09ce86d7aab23a
anapedra <anapedra.mil@gmail.com>
2024-03-29 02:14:03 -0300

    Project School created

---
+++ backend/school-project/src/test/java/com/anapedra/schoolproject/SchoolProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anapedra.schoolproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
